7bad788277cff5c3821578a1bd75c2442a05c31e
Pedro Garcia <coolcatdopedog@gmail.com>
2024-10-30 19:13:40 -0700

    added function that overflows and function that calls another function

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function circumference(diameter) {
 
 console.log(circumference(12));
 
+function balloon() {
+    console.log('the balloons are coming');
+    monkey();
+    console.log('*balloon popping noises')
+
+}
+
+function monkey() {
+    console.log('monkey spots balloons')
+}
+
+balloon()
+
+function java() {
+    console.log('JavaScript')
+    java()
+}
+
+java()

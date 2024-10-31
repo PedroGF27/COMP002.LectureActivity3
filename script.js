15aeda1f6d83f346fb4f985aa0997f9c00989a23
Pedro Garcia <coolcatdopedog@gmail.com>
2024-10-30 18:51:41 -0700

    added function to give circumference

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function greet(hello) {
-    return 
+function greet(name) {
+    console.log('Hello, ' + name);
 }
+(greet("Pedro"));
+
+function circumference(diameter) {
+    const pi = Math.PI;
+    return pi * diameter;
+}
+
+console.log(circumference(12));
+
